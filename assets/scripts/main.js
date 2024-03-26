5ab510e04615fb658024a95af0d15544818e52eb
martro96 <martarodrigo.55@gmail.com>
2024-03-26 14:21:51 +0100

    añado diseño ubicacion

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,3 +11,41 @@
  */
 import { register } from 'swiper/element/bundle';
 register();
+
+
+let swiperContainer = document.querySelector('.swiper__wrapper');
+let swiper = new swiper(swiperContainer, {
+    slidesPerView: 3,
+    gridRows: 1,
+    mousewheelForceToAxis: true,
+});
+
+window.addEventListener('resize', function() {
+    if (window.innerWidth < 768) { 
+        swiper.params.slidesPerView = 1;
+        swiper.update(); 
+    } else {
+        swiper.params.slidesPerView = 3;
+        swiper.update();
+    }
+});
+
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+const enlacesCerrarMenu = document.querySelectorAll(".main-nav__link");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+});
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+});
+
+enlacesCerrarMenu.forEach(enlace => {
+    enlace.addEventListener("click", () => {
+        nav.classList.remove("visible");
+    });
+});
